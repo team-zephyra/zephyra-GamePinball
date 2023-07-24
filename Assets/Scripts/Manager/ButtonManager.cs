77045af256741873703c3a1036455e54e7e22a73
rizaldysdw <rizaldysdw@gmail.com>
2023-07-24 23:40:34 +0700

    Features Addition - Problem Solving 1
    - Ball respawn to spawn position if it passes the paddles
    - Bumper changes color if hit by the ball
    - Coin spawn over time (max 3 coins in the scene)
    - Add Exit Button

---
+++ Assets/Scripts/Manager/ButtonManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
